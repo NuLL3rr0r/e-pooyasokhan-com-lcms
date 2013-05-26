020dd797d92553510c2b80a5ce1826ae0b128f1e
M.S. Babaei <info@babaei.net>
2013-05-27 00:07:20 +0430

    Added build scripts for FreeBSD (gcc-4.6+) and Windows (MinGW).

---
+++ _build-srcipts/freebsd-gcc46.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir ..\build
+cd build
+cmake -DCMAKE_C_COMPILER=/usr/local/bin/gcc46 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++46 -D CMAKE_BUILD_TYPE=GCCNativeMaxSpeedRel ../
+
---
+++ _build-srcipts/mingw-debug.cmd
@@ -0,0 +1,7 @@
+mkdir build
+cd ..\build
+
+cmake -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Debug -DCMAKE_INCLUDE_PATH=deps-win32\mingw\include -DCMAKE_LIBRARY_PATH=deps-win32\mingw\lib\debug -DCMAKE_FRAMEWORK_PATH=deps-win32\mingw\share -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=deps-win32\mingw\Boost ..\
+
+REM cmake -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.0.2\5.0.2\mingw47_32\lib\cmake\Qt5Core -DCMAKE_INCLUDE_PATH=deps-win32\mingw\include -DCMAKE_LIBRARY_PATH=deps-win32\mingw\lib\debug -DCMAKE_FRAMEWORK_PATH=deps-win32\mingw\share -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=deps-win32\mingw\Boost ..\
+
