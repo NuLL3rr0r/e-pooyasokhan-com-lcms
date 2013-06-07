366ddc0fa3f62cebc507ef560a253d1768d084e5
M.S. Babaei <info@babaei.net>
2013-06-07 06:41:32 +0430

    missing file from last commit

---
+++ product-info.cmake
@@ -0,0 +1,6 @@
+SET ( EPS_DESKTOP_COPYRIGHT_HOLDER "PooyaSokhan(R) English Institute" )
+SET ( EPS_DESKTOP_COPYRIGHT_YEAR "(C) 2013" )
+SET ( EPS_DESKTOP_DEVELOPER "M.S. Babaei (info@babaei.net)" )
+SET ( EPS_DESKTOP_PRODUCT_NAME "E-PooyaSokhan.com Desktop Version" )
+SET ( EPS_DESKTOP_PRODUCT_VERSION "0.1.153.12" )
+
