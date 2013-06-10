3d38340d221a71cfbf238d41dd5a0eaffb5ffcb4
M.S. Babaei <info@babaei.net>
2013-06-10 08:54:52 +0430

    Minor fix for linking against static winpthread for the launcher

--- _win32-portable/native_launcher-build.bat
+++ _win32-portable/native_launcher-build.bat
@@ -13,7 +13,8 @@ chcp 1252
 windres -O coff %LAUNCHER_RC% -o %LAUNCHER_OUTPUT_RES%
 
 REM Note: Instead of "-mwindows" or "-mconsole", it is possible to add "-subsystem,windows" or "-subsystem,console" to linker flags.
-g++ -Wall -Wextra -Wno-unused-parameter -pedantic -pipe -Os -fomit-frame-pointer -static-libgcc -static-libstdc++ -mwindows -Wl,--gc-sections -ffunction-sections -fdata-sections -o %LAUNCHER_OUTPUT_APP% %LAUNCHER_OUTPUT_RES% %LAUNCHER_SOURCE%
+REM "-static" is added for winpthread and other needed libs if any
+g++ -Wall -Wextra -Wno-unused-parameter -pedantic -pipe -Os -fomit-frame-pointer -static-libgcc -static-libstdc++ -static -mwindows -lwinpthread -Wl,--gc-sections -ffunction-sections -fdata-sections -o %LAUNCHER_OUTPUT_APP% %LAUNCHER_OUTPUT_RES% %LAUNCHER_SOURCE%
 
 strip %LAUNCHER_OUTPUT_APP%
 strip -R.comment %LAUNCHER_OUTPUT_APP%
