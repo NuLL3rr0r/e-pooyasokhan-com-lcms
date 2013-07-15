2ff60a4c378e8b27824d2bd3095509fc151ffa75
M.S. Babaei <info@babaei.net>
2013-07-15 15:16:48 +0430

    Added git to cmake scripts Handling build macros differently Automatic Version and Revisions Missing files from last commit

--- resources.cmake
+++ resources.cmake
@@ -1,5 +1,8 @@
 SET ( EPS_SERVER_RC_FILE "resources/app.rc" )
+SET ( EPS_SERVER_APP_ICON_FILE "resources/app-icon.ico" )
+
 SET ( EPS_DESKTOP_RC_FILE "resources/app.rc" )
+SET ( EPS_DESKTOP_APP_ICON_FILE "resources/app-icon.ico" )
 
 SET ( EPS_DESKTOP_QRC_FILE "resources/resources.qrc" )
 
