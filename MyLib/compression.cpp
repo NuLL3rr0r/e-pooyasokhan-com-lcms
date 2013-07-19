da24b96f9b9020a5cfc18c0a6c6c74355e3aa86e
M.S. Babaei <info@babaei.net>
2013-07-19 14:25:50 +0430

    Minor fix in compression module

--- MyLib/compression.cpp
+++ MyLib/compression.cpp
@@ -2,6 +2,10 @@
 #include <boost/iostreams/filter/zlib.hpp>
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include "compression.hpp"
+#include "log.hpp"
+
+#define     COMP_ERROR          "Unknow compression error!";
+#define     DECOMP_ERROR        "Unknow decompression error!";
 
 using namespace boost;
 using namespace MyLib;
@@ -16,12 +20,16 @@ void Compression::Compress(const std::string &dataString,
 void Compression::Compress(const CompressionBuffer_t &dataBuffer,
                            CompressionBuffer_t &out_compressedBuffer)
 {
-    out_compressedBuffer.clear();
+    try {
+        out_compressedBuffer.clear();
 
-    iostreams::filtering_streambuf<iostreams::output> output;
-    output.push(iostreams::zlib_compressor());
-    output.push(iostreams::back_inserter(out_compressedBuffer));
-    iostreams::write(output, &dataBuffer[0], dataBuffer.size());
+        iostreams::filtering_streambuf<iostreams::output> output;
+        output.push(iostreams::zlib_compressor());
+        output.push(iostreams::back_inserter(out_compressedBuffer));
+        iostreams::write(output, &dataBuffer[0], dataBuffer.size());
+    } catch(...) {
+        LOG_ERROR(COMP_ERROR)
+    }
 }
 
 void Compression::Decompress(const CompressionBuffer_t &dataBuffer,
@@ -35,11 +43,15 @@ void Compression::Decompress(const CompressionBuffer_t &dataBuffer,
 void Compression::Decompress(const CompressionBuffer_t &dataBuffer,
                              CompressionBuffer_t &out_uncompressedBuffer)
 {
-    out_uncompressedBuffer.clear();
+    try {
+        out_uncompressedBuffer.clear();
 
-    iostreams::filtering_streambuf<iostreams::output> output;
-    output.push(iostreams::zlib_decompressor());
-    output.push(iostreams::back_inserter(out_uncompressedBuffer));
-    iostreams::write(output, &dataBuffer[0], dataBuffer.size());
+        iostreams::filtering_streambuf<iostreams::output> output;
+        output.push(iostreams::zlib_decompressor());
+        output.push(iostreams::back_inserter(out_uncompressedBuffer));
+        iostreams::write(output, &dataBuffer[0], dataBuffer.size());
+    } catch(...) {
+        LOG_ERROR(DECOMP_ERROR)
+    }
 }
 
